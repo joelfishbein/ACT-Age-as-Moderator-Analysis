25328758533c86214c31e6ffdb9b16597a4e16f7
WeLikeTheStat <we.like.the.stat@gmail.com>
2022-11-03 07:57:00 -0700

    add paper planning script

---
+++ Paper planning.R
@@ -0,0 +1,14 @@
+
+
+# Aim 1, inspired by previous work (partial replication)
+1. Older adults improve more on pain severity during intervention, get worse more after
+2. Older adults improve more on pain interference during intervention, get worse more after
+
+# Potential covariates of change in pain
+3. CPAQ - no difference in amt of change by age
+4. Depression - Older adults improve marginally more more during intervention, no difference in FU slopes
+5. Sleep - no difference in amt of change by age
+
+# Exploratory Potential mechanistic model
+6. Age -> Change in depression -> Change in pain severity (over Pre-Post)
+7. Age -> Change in depression -> Change in pain interference (over Pre-Post)
