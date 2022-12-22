33f4b688a0d74f40deb01bca96fe38aa1b6595ac
WeLikeTheStat <we.like.the.stat@gmail.com>
2022-12-22 08:34:37 -0800

    update

--- Paper planning.R
+++ Paper planning.R
@@ -1,6 +1,6 @@
 
 
-# Aim 1, inspired by previous work (partial replication)
+# Examine moderation of pain outcomes by age
 1. Older adults improve more on pain severity during intervention, get worse more after
 2. Older adults improve more on pain interference during intervention, get worse more after
 
@@ -9,6 +9,6 @@
 4. Depression - Older adults improve marginally more more during intervention, no difference in FU slopes
 5. Sleep - no difference in amt of change by age
 
-# Exploratory Potential mechanistic model
-6. Age -> Change in depression -> Change in pain severity (over Pre-Post)
-7. Age -> Change in depression -> Change in pain interference (over Pre-Post)
+# Demonstrating depression as a covariate of change in pain interference but not severity
+6. Change in depression -> Change in pain severity (over Pre-Post)
+7. Change in depression -> Change in pain interference (over Pre-Post)
